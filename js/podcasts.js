93bf94ac7d7ee4ae7f475255eeb4ef559f7010b9
lehmaria <leticiamps@live.com>
2020-07-22 19:51:06 -0300

    Radio Podcast v1

---
+++ js/podcasts.js
@@ -0,0 +1,32 @@
+const realPodcasts = [
+    {
+        id: '88.5',
+        title:'Loop Matinal',
+        description: 'Notícias sobre tecnologia',
+    },
+    {
+        id: '95.8',
+        title:'Hipsters.tech',
+        description: 'Desenvolvimento em geral',
+    },
+    {
+        id: '99.6',
+        title:'Área de Transferência',
+        description: 'Tecnologia em geral',
+    },
+    {
+        id: '100.4',
+        title:'Nerdcast',
+        description: 'Cultura nerd em geral',
+    },
+    {
+        id: '100.8',
+        title:'DioCast',
+        description: 'Tecnologia e desenvolvimento em geral',
+    },
+    {
+        id: '101.3',
+        title:'Devs Cansados',
+        description: 'Desenvolvimento em geral',
+    },
+];
---
+++ js/script.js
@@ -0,0 +1,36 @@
+let inputDisplay = null;
+let inputRange = null;
+
+window.addEventListener('load', start);
+
+function start() {
+    inputDisplay = document.querySelector('#inputDisplay');
+    inputRange = document.querySelector('#inputRange');
+    divPodcast = document.querySelector('.podcast');
+
+    inputRange.addEventListener('input', handleRangeInput);
+
+    renderPodcastFrom(inputRange.value);
+}
+
+function handleRangeInput(event) {
+    const frequency = event.target.value;
+    inputDisplay.value = frequency;
+
+    renderPodcastFrom(frequency);
+}
+
+function renderPodcastFrom(frequency) {
+    divPodcast.innerHTML = '';
+
+    const podcast = realPodcasts.find((podcast) => podcast.id === frequency);
+
+    if (!podcast) {
+        divPodcast.innerHTML = `<p> Nenhum podcast encontrado </p>`;
+    } else {
+        divPodcast.innerHTML = `<h2>${podcast.title} </h2>
+        <span> ${podcast.description} </span>`;
+    }
+
+
+}
